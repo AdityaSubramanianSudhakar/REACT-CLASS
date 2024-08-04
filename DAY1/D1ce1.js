aeda09169378a786ce6b12a51793666d1f5bdf03
Aditya Subramanian S <144915630+AdityaSubramanianSudhakar@users.noreply.github.com>
2024-08-04 11:50:04 +0530

    Add files via upload

---
+++ DAY1/D1ce1.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function alertMsg(){
+  alert("Message from Javascript Alert!");
+  console.log("Message to developer");
+}
+function Day1ce1() {
+  return (
+    <div>
+        <h1>Let we see the output of JAVASCRIPT</h1>
+        <button onClick={alertMsg}>Click</button>
+        </div>
+  );
+}
+
+export default Day1ce1;
+
